09683b270ab7a83c6edd9d5215f5a779f6dc21a9
Willy <wacb14@gmail.com>
2025-02-09 21:23:07 -0500

    Hook useState and batching

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import { useState } from 'react';
 import { Button } from './components';
 function App() {
-  function countMore() {
+  // Batching
+  function countFive() {
     setCount(count => count + 1);
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+    // Al ejecutar como una función si actualiza el valor previo
+  }
+  function countOne() {
+    setCount(count + 1);
+    setCount(count + 1);
+    setCount(count + 1);
+    setCount(count + 1);
+    setCount(count + 1);
+    setCount(count + 1);
+    setCount(count + 1);
+    // Aqui se agrupan los cambios de estado y no se actualiza el valor hasta que haya un render
   }
   const [count, setCount] = useState(0);
   return (
     <>
-      <Button label={`Count is: ${count}`} parentMethod={countMore} />
+      <h1>Count is: {count}</h1>
+      <Button label={`Count One`} parentMethod={countOne} />
+      <Button label={`Count Five`} parentMethod={countFive} />
     </>
   );
 }
